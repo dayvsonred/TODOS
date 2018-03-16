cae810fdc3ea3203d415ab643d3239a609b48b24
dayvsonred <dayvson.red@gmail.com>
2018-03-16 17:40:22 -0300

    subindo projeto  api MVC

---
+++ API_CRUD_Entity_Framework/Api/Api/DataContexts/UsuarioContext.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Data.Entity;
+using Api.Models;
+
+namespace WebAppi.Contexts
+{
+    public class UsuarioContext : DbContext
+    {
+        public UsuarioContext() : base("Conn_BD_TODOS") { }
+
+        public DbSet<Usuario> Usuario { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+
+            base.OnModelCreating(modelBuilder);
+        }
+
+    }
+}
---
+++ API_CRUD_Entity_Framework/Api/Api/Models/Usuario.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Api.Models
+{
+    public class Usuario
+    {
+        [Key]
+        public int ID_USUARIO { get; set; }
+        public string LOGIN { get; set; }
+        public string NOME { get; set; }
+        public string EMAIL { get; set; }
+        public string SENHA { get; set; }
+        public int ATIVO { get; set; }
+        public DateTime DT_INCLUSAO { get; set; }
+    }
+}
